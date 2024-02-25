3d624eba2553e35c6c61f74a4b6b6e001cd779aa
Emircan Çapkan <99869455+emircancapkan@users.noreply.github.com>
2024-02-25 15:10:39 +0300

    Add files via upload

---
+++ demo/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.sql;
+    requires com.fasterxml.jackson.databind;
+
+    opens com.example.demo to javafx.fxml;
+    exports com.example.demo;
+    exports com.example.demo.models;
+    opens com.example.demo.models to javafx.fxml;
+}
